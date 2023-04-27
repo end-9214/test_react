d4c4fabc597d4fe44e9ebd77b92b648b810ce464
END <karamveersingh2003111@Gmail.com>
2023-04-27 14:01:37 +0000

    commit

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,16 +1,58 @@
 import React from 'react'
 import styled from "styled-components";
 
+const data = [
+  "Web Design",
+  "Development",
+  "Illustration",
+  "Product Design",
+  "Social Media",
+
+]
 
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
-`
+  display: flex;
+  justify-content: center;
+
+`;
+const Container = styled.div`
+width: 1400px;
+display: flex;
+justify-content: space-between;
+`;
+
+const Left = styled.div`
+flex: 1;
+
+`;
+
+const List = styled.ul``;
+
+const ListItem = styled.li``;
+
+const Right = styled.div`
+flex: 1;
+
+`;
+
 
 const Works = () => {
   return (
-    <Section>Works</Section>
+    <Section>
+      <Container>
+        <Left>
+          <List>
+            {data.map((item) => (
+              <ListItem key={item}>{item}</ListItem>
+            ))}
+          </List>
+        </Left>
+        <Right></Right>
+      </Container>
+    </Section>
   )
 }
 
-export default Works
+export default Works;
