9c71d31a25430a5482b32fe7abd68a2ef8063502
END <karamveersingh2003111@Gmail.com>
2023-04-27 10:00:51 +0000

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,17 +6,41 @@ const Section = styled.div`
   display: flex;
   justify-content: center;
 
-`
-const Container= styled.div`
+`;
+const Container = styled.div`
   width: 1400px;
-  backgroud-color: purple;
+  background-color: rebeccaPurple;
 
-`
+
+`;
+const Links = styled.div``;
+const Logo = styled.img``;
+const Lists = styled.ul``;
+const ListItems = styled.li``;
+const Icons = styled.div``;
+const Icon = styled.img``;
+const Button = styled.button``;
 
 const Navbar = () => {
   return (
     <Section>
-      <Container>Navbar</Container>
+      <Container>
+        <Links>
+        <Logo src="./img/logo.png"/>
+        <Lists>
+          <ListItems>Home</ListItems>
+          <ListItems>About</ListItems>
+          <ListItems>Works</ListItems>
+          <ListItems>Contact</ListItems>
+
+        </Lists>
+        </Links>
+
+        <Icons>
+          <Icon src="./img/search.png"/>
+          <Button>Hire Now</Button>
+        </Icons>
+      </Container>
     </Section>
   )
 }
