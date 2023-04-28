273d60cf7a366b005d2ba18f30ab3442f61c269d
END <karamveersingh2003111@Gmail.com>
2023-04-28 07:02:31 +0000

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -119,7 +119,7 @@ const Hero = () => {
       <OrbitControls enableZoom={false} />
       <ambientLight intensity={1} />
       <directionalLight position={[3,2,1]}/>
-        <Sphere args={[1,100,200]} scale={1.5}>
+        <Sphere args={[1,100,200]} scale={2.5}>
         <MeshDistortMaterial color="#450c70" attach="material" distort={0.5} speed={2}/>
         </Sphere>
       </Canvas>
